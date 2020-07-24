f9d1fdb9060c26f5443471d3dff6cbc0c7a71270
Leticia Massenan <massenandev@gmail.com>
2020-07-24 00:55:39 -0300

    initial artifacts and user domain

---
+++ src/test/java/br/com/massenan/supportportal/SupportPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.massenan.supportportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
